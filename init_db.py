f8c1691e55276b6fdc010c45861caf2e521e909e
Xodjayev-aa <xoja10122012@gmail.com>
2026-08-31 08:43:32 +0500

    init_db.py

---
+++ init_db.py
@@ -0,0 +1,12 @@
+import asyncio
+from database import engine
+from models import Base
+
+async def create_tables():
+    print("Creating database tables...")
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    print("Done! Database is ready.")
+
+if __name__ == "__main__":
+    asyncio.run(create_tables())
